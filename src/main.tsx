c2b7f3bde282e102c1ef9f78d530d3ebe29a2cd0
Jocais Ampudia <ampudiaflores@gmail.com>
2026-03-09 15:39:47 -0500

    feat(main): wrap App component with ConfigProvider for localization support

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,6 @@
+import { ConfigProvider } from "antd";
 import "antd/dist/reset.css";
+import esES from "antd/locale/es_ES";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
@@ -6,6 +8,8 @@ import "./shared/styles/global.less";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ConfigProvider locale={esES}>
+      <App />
+    </ConfigProvider>
   </React.StrictMode>
 );
